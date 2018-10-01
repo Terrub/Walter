69973c91652013508ac3b7190323f3c14d2c25a7
Robert Page <page.synid@gmail.com>
2018-10-01 21:57:05 +0200

    Refactors Walter's alive message even further.

--- Walter.lua
+++ Walter.lua
@@ -1,20 +1,24 @@
 -- Walter AddOn!
 
+--------------------------------------------------------------------------------
+-- UTIL FUNCTIONS,
+-- agnostic of walter's existance
+--------------------------------------------------------------------------------
 local function prt(str)
     DEFAULT_CHAT_FRAME:AddMessage(str)
 end
 
-local function print_addon_string(name, lbl, msg)
+Walter = {}
+
+function Walter.print_addon_string(name, lbl, msg)
     local pattern = "|cff22ff22%s|r - |cff999999%s:|r %s"
     local str = string.format(pattern, name, lbl, msg)
 
     prt(str)
 end
 
-Walter = {}
-
-function Walter.say(message)
-    print_addon_string('walter', 'says', message)
+local function say(message)
+    Walter.print_addon_string('walter', 'says', message)
 end
 
-Walter.say("I'm ALIVE!")
+say("I'm ALIVE!")
