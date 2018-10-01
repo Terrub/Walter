374508efeb1f36c87e26333ca9ee9580e5b99674
Robert Page <page.synid@gmail.com>
2018-10-01 21:18:06 +0200

    Adds Walter lua and toc files.

---
+++ Walter.lua
@@ -0,0 +1,7 @@
+-- Walter AddOn!
+
+local label = "says"
+local message = "I'm ALIVE!"
+local str = "|cff22ff22Walter|r - |cff999999" .. label .. ":|r " .. message
+
+DEFAULT_CHAT_FRAME:AddMessage(str)
