3808ac16b268a07497359ee260e84220e333e5b6
Robert Page <page.synid@gmail.com>
2018-10-02 12:46:21 +0200

    Adds dependency injection to Walter initiation using config.

--- Walter.lua
+++ Walter.lua
@@ -1,4 +1,4 @@
--- Walter AddOn!
+-- ToDo: Add license!!
 
 --------------------------------------------------------------------------------
 -- UTIL FUNCTIONS,
@@ -8,17 +8,49 @@ local function prt(str)
     DEFAULT_CHAT_FRAME:AddMessage(str)
 end
 
+local function initialise_addon_with_config(addon, config)
+	for key, val in pairs(config) do
+		local crouton_name = string.format("create_%s", key)
+        local crouton = addon[crouton_name]
+        if crouton then
+            addon[key] = crouton(val)
+        end
+    end
+end
+
+--------------------------------------------------------------------------------
+-- Meet Walter!
+--------------------------------------------------------------------------------
 Walter = {}
 
-function Walter.print_addon_string(name, lbl, msg)
-    local pattern = "|cff22ff22%s|r - |cff999999%s:|r %s"
-    local str = string.format(pattern, name, lbl, msg)
-
-    prt(str)
-end
-
+--------------------------------------------------------------------------------
+-- Walter's private stuff.
+--------------------------------------------------------------------------------
 local function say(message)
-    Walter.print_addon_string('walter', 'says', message)
+    Walter.print('Walter', 'says', message)
 end
+
+--------------------------------------------------------------------------------
+-- Walter's public stuff.
+--------------------------------------------------------------------------------
+function Walter.create_print(print_func)
+    local function _print(name, lbl, msg)
+        local pattern = "|cff22ff22%s|r - |cff999999%s:|r %s"
+        local str = string.format(pattern, name, lbl, msg)
+    
+        print_func(str)
+    end
+    
+    return _print
+end
+
+--------------------------------------------------------------------------------
+-- Walter's intiation procedure.
+-------------------------------------------------------------------------------- 
+local config = {
+	["print"] = prt
+}
+
+initialise_addon_with_config(Walter, config)
 
 say("I'm ALIVE!")
